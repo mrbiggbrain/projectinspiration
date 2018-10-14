d0396ed24f1b302a8de4e23de6192bb1792f9491
Nicholas Young <mrbiggbrain@gmail.com>
2018-10-13 21:35:38 -0400

    Move code out of try block

--- Clients/Bots/DiscordBot/Commands/RollCommands.cs
+++ Clients/Bots/DiscordBot/Commands/RollCommands.cs
@@ -4,14 +4,10 @@ using System.Threading.Tasks;
 using System.Text;
 using Discord;
 using Discord.Commands;
-using ProjectInspiration.SDK.Dice;
-using ProjectInspiration.Library.Dice.Models;
-using ProjectInspiration.Library.Dice;
 using DiscordBot.Formatters;
 using ProjectInspiration.SDK.Shared.TransactionService;
 using ProjectInspiration.Library.Dice.Models.Request;
 using ProjectInspiration.Library.Dice.Models.Responce;
-using System.Linq;
 using Newtonsoft.Json;
 
 namespace DiscordBot.Commands
@@ -27,29 +23,37 @@ namespace DiscordBot.Commands
             // Create a new service to use for our web request.
             ITransactionService service = new ProjectInspirationWebService("https://localhost:44319/api", "");
 
+            // Create variable to hold results of query. 
+            IRollResult result;
+
             try
             {
-                IRollResult result = service.Post<RollResult, IRollRequest>(request, "dice");
-
-                EmbedBuilder builder = new EmbedBuilder();
-
-                builder.WithAuthor("Dice Roller", "https://cdn.pixabay.com/photo/2017/08/31/04/01/d20-2699387_960_720.png");
-                builder.WithColor(Color.Red);
-                builder.WithDescription($"{Context.User.Mention} Rolled a {result.Value}\n{DiscordFormatter.Format(request.RollText, bold: true, underline: true, italics: true)} :: {RollResponceFormatter.Format(result)}");
-
-                await Context.Channel.SendMessageAsync("", embed: builder.Build());
-                await Context.Message.DeleteAsync();
+                // Query the Transaction Service to get the result. 
+                result = service.Post<RollResult, IRollRequest>(request, "dice");     
             }
-            catch (Exception e)
+            catch (JsonSerializationException e)
             {
+                // Output errors with JSON serialization/Deserialization
                 Console.WriteLine($"Issue: {e.Message}");
             }
-            
 
-            
+            // Create a builder to construct the output. 
+            EmbedBuilder builder = new EmbedBuilder();
 
-            
+            // Set author and image
+            builder.WithAuthor("Dice Roller", "https://cdn.pixabay.com/photo/2017/08/31/04/01/d20-2699387_960_720.png");
 
+            // Set outline to be red.
+            builder.WithColor(Color.Red);
+
+            // Set the description to show results. 
+            builder.WithDescription($"{Context.User.Mention} Rolled a {result.Value}\n{DiscordFormatter.Format(request.RollText, bold: true, underline: true, italics: true)} :: {RollResponceFormatter.Format(result)}");
+
+            // Post the embed to the channel
+            await Context.Channel.SendMessageAsync("", embed: builder.Build());
+
+            // Delete the requesting message
+            await Context.Message.DeleteAsync();
         }
     }
 }
