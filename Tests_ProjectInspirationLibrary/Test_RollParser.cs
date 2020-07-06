3d1038c1f262546ddc9f32f8969f1369e39cfb7e
Nicholas Young <mrbiggbrain@gmail.com>
2020-07-06 14:16:42 -0400

    Fixed Spelling for Comments.

--- Tests_ProjectInspirationLibrary/Test_RollParser.cs
+++ Tests_ProjectInspirationLibrary/Test_RollParser.cs
@@ -1,15 +1,30 @@
-using NUnit.Framework;
-using ProjectInspirationLibrary.Dice.Parser;
-
+//-----------------------------------------------------------------------
+// <copyright file="Test_RollParser.cs" company="Project Inspiration">
+//     Copyright (c) Nicholas A Young. All rights reserved.
+// </copyright>
+// <author>Nicholas A. Young</author>
+//-----------------------------------------------------------------------
 namespace Tests_ProjectInspirationLibrary
 {
+    using NUnit.Framework;
+    using ProjectInspirationLibrary.Dice.Parser;
+
+    /// <summary>
+    /// Tests for ProjectInspirationLibrary.Dice.Parser.RollParser
+    /// </summary>
     public class Test_RollParser
     {
+        /// <summary>
+        /// Perform setup steps for tests.
+        /// </summary>
         [SetUp]
         public void Setup()
         {
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRoll_IsTrue()
         {
@@ -17,6 +32,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing capital letters returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollCaps_IsTrue()
         {
@@ -24,6 +42,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if garbage text returns as valid. Should be false.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesGarbage_IsFalse()
         {
@@ -31,6 +52,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsFalse(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing the ADV keyword returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollAdvantage_IsTrue()
         {
@@ -38,6 +62,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing the DIS keyword returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollDisadvantage_IsTrue()
         {
@@ -45,6 +72,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing the KL keyword returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollKeepLow_IsTrue()
         {
@@ -52,6 +82,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing the KL keyword with no value returns as valid. Should be false.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollKeepLowNoValue_IsFalse()
         {
@@ -59,6 +92,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsFalse(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing the KH keyword returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollKeepHigh_IsTrue()
         {
@@ -66,6 +102,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but containing the KH keyword with no value returns as valid. Should be false.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollKeepHighNoValue_IsFalse()
         {
@@ -73,6 +112,9 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsFalse(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but with the ADV keyword in caps returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesSimpleRollAdvantageCaps_IsTrue()
         {
@@ -80,13 +122,19 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a complex dice roll but with various dice types, keywords, simple numbers, and comments returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesComplexRoll_IsTrue()
         {
-            bool result = RollParser.Check("1d20 + 7d8 adv + 4d8 dis + 6 + 88d100 + 4D8 + 6d22 DIS + 7d4 KH2 + 8d4 Kl4");
+            bool result = RollParser.Check("1d20 + 7d8 adv + 4d8 dis + 6 + 88d100 + 4D8 + 6d22 DIS + 7d4 KH2 + 8d4 Kl4 # Hello!");
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but with the two kinds of dice returns as valid. Should be true.
+        /// </summary>
         [Test]
         public void RollParser_Check_ValidatesDoubleRoll_IsTrue()
         {
@@ -94,5 +142,34 @@ namespace Tests_ProjectInspirationLibrary
             Assert.IsTrue(result);
         }
 
+        /// <summary>
+        /// Validates if text representing a simple dice roll but with a comment returns as valid. Should be true.
+        /// </summary>
+        [Test]
+        public void RollParser_Check_ValidatesComment_IsTrue()
+        {
+            bool result = RollParser.Check("1d20 #Test");
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Validates if text representing a simple number returns as valid. Should be true.
+        /// </summary>
+        [Test]
+        public void RollParser_Check_ValidatesNumber_IsTrue()
+        {
+            bool result = RollParser.Check("1337");
+            Assert.IsTrue(result);
+        }
+
+        /// <summary>
+        /// Validates if text representing a simple dice roll but with incorrect letter for separator returns as valid. Should be false.
+        /// </summary>
+        [Test]
+        public void RollParser_Check_ValidatesDoubleRoll_IsFalse()
+        {
+            bool result = RollParser.Check("1x20");
+            Assert.IsFalse(result);
+        }
     }
 }
